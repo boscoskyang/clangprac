762f94811c458cdc2fe161ed24c134c87fca46c9
Coursera Learner <nobody@nowhere.nul>
2019-02-18 01:08:45 +0000

    fix 14

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * arrayMax(int * array, int n) {
+int * arrayMax(int * array, size_t n) {
   int * maxie;
   if (n == 0 || array == NULL){
     maxie = NULL;
