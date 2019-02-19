79feffdd509aaa97973c03bd88928d3144d05160
Coursera Learner <nobody@nowhere.nul>
2019-02-19 01:04:05 +0000

    fix assign 16

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -4,19 +4,27 @@
 size_t maxSeq(int * array, size_t n){
   size_t currMax = 0;
   size_t allMax;
+  size_t currSeq = 0;
   if (n == 0){
     allMax = currMax;
   }
 
   else{
-       allMax = 1; 
+       allMax = 1;
+       currMax = 1;
+       currSeq = 1;
     for (int i = 0; i < (n-1); i++){
       if (array[i] < array[i+1]){
-	currMax++;
-	allMax = currMax;
+	currSeq++;
+	currMax = currSeq;
+
+	if(currMax > allMax){
+	  allMax = currMax;
+	}
       }
       else{
-	currMax = 1;
+	currSeq =1;
+	currMax = currSeq;
       }
     }
   }
