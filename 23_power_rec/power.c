bffb5e57a9118e3af26b269d9e353e9bff6c71ac
Coursera Learner <nobody@nowhere.nul>
2019-02-24 14:38:25 +0000

    did assign 23

--- 23_power_rec/power.c
+++ 23_power_rec/power.c
@@ -2,11 +2,11 @@
 #include<stdlib.h>
 
 unsigned power(unsigned x, unsigned y){
-  if (x == 0){
-    return 0;
-  }
   if (y == 0){
     return 1;
+  }
+  if (x == 0){
+    return 0;
   }
 
     return x*power(x,y-1);
